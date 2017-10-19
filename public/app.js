4680cd3f180fd955afde3640757acb59ed09b1cc
Petteri Lapinlampi <Petteri.Lapinlampi@gmail.com>
2017-10-19 11:36:22 +0000

    Add warehouse and warehouse freezer temperatures

--- public/app.js
+++ public/app.js
@@ -1,19 +1,29 @@
-var config = {
+var lineChartConfigFMI = {
   type: 'line',
   data: {
-    datasets: [{
-      label: place,
-      backgroundColor: 'rgba(240, 100, 100, 0.5)',
-      borderColor: 'rgba(240, 100, 100, 1)',
-      fill: false,
-      pointRadius: 0,
-      data: dataset,
-    }],
+    datasets: [
+      {
+        label: fmiLocation1,
+        backgroundColor: 'rgba(240, 100, 100, 0.5)',
+        borderColor: 'rgba(240, 100, 100, 1)',
+        fill: false,
+        pointRadius: 0,
+        data: fmiDataset1
+      },
+      {
+        label: 'Warehouse',
+        backgroundColor: 'rgba(100, 100, 240, 0.5)',
+        borderColor: 'rgba(100, 100, 240, 1)',
+        fill: false,
+        pointRadius: 0,
+        data: warehouseDataset
+      }
+    ]
   },
   options: {
     responsive: true,
     title:{
-      text:'Temperatures'
+      text:'Temperature from FMI'
     },
     tooltips: {
       mode: 'index',
@@ -38,7 +48,57 @@ var config = {
   }
 };
 
+var lineChartConfigWarehouseFreezer = {
+  type: 'line',
+  data: {
+    datasets: [
+      {
+        label: 'Warehouse freezer',
+        backgroundColor: 'rgba(100, 240, 100, 0.5)',
+        borderColor: 'rgba(100, 240, 100, 1)',
+        fill: false,
+        pointRadius: 0,
+        data: warehouseFreezerDataset
+      }
+    ]
+  },
+  options: {
+    responsive: true,
+    title:{
+      text:'Temperature from our warehouse freezer'
+    },
+    tooltips: {
+      mode: 'index',
+      intersect: false
+    },
+    scales: {
+      xAxes: [{
+        type: 'time',
+        distribution: 'linear',
+        time: {
+          unit: 'day',
+          tooltipFormat: 'll HH:mm'
+        }
+      }],
+      yAxes: [{
+        scaleLabel: {
+          display: true,
+          labelString: 'Temperature [C]'
+        },
+        ticks: {
+          max: -18,
+          min: -24
+        }
+      }]
+    }
+  }
+};
+
+
 window.onload = function() {
-  var ctx = document.getElementById("myChart").getContext("2d");
-  window.myLine = new Chart(ctx, config);
+  var ctx1 = document.getElementById("lineChartFMI").getContext("2d");
+  window.myLine = new Chart(ctx1, lineChartConfigFMI);
+
+  var ctx2 = document.getElementById("lineChartWarehouseFreezer").getContext("2d");
+  window.myLine = new Chart(ctx2, lineChartConfigWarehouseFreezer);
 };
--- routes/temperature.js
+++ routes/temperature.js
@@ -4,22 +4,49 @@ var router = express.Router();
 
 const db = require('../db');
 
-router.use(function queryData (req, res, next) {
-  const startTime = moment().add(-7, 'd').format('YYYY-MM-DDTHH:mm:ss');
-  const sql = "SELECT (time) AS x, (temperature) AS y FROM temperature WHERE time >= $1";
+const startTime = moment().add(-7, 'd').format('YYYY-MM-DDTHH:mm:ss');
+
+router.use(function queryFmiData (req, res, next) {
+  const sql = "SELECT (time) AS x, (temperature) AS y FROM temperature WHERE time >= $1 AND location = 'Kirkkonummi';"
   db.query(sql, [startTime], (err, result) => {
     if (err) {
       console.log(err.stack);
     } else {
-      req.dataset = result.rows;
+      req.fmiDataset1 = result.rows;
       next();
     }
   })
 })
 
+router.use(function queryWarehouseData (req, res, next) {
+  const sql = "SELECT (time) AS x, (temperature) AS y FROM temperature WHERE time >= $1 AND location = 'Varasto';"
+  db.query(sql, [startTime], (err, result) => {
+    if (err) {
+      console.log(err.stack);
+    } else {
+      req.warehouseDataset = result.rows;
+      next();
+    }
+  })
+})
+
+router.use(function queryWarehouseFreezerData (req, res, next) {
+  const sql = "SELECT (time) AS x, (temperature) AS y FROM temperature WHERE time >= $1 AND location = 'Pakastin_varasto';"
+  db.query(sql, [startTime], (err, result) => {
+    if (err) {
+      console.log(err.stack);
+    } else {
+      req.warehouseFreezerDataset = result.rows;
+      next();
+    }
+  })
+})
+
+
 router.get('/', function (req, res) {
-  res.render('temperature_chart', { title: 'Temperature chart', place: 'Kirkkonummi',
-    dataset: req.dataset });
+  res.render('temperature_chart', { title: 'Temperature chart', fmiLocation1: 'Kirkkonummi',
+    fmiDataset1: req.fmiDataset1, warehouseDataset: req.warehouseDataset,
+    warehouseFreezerDataset: req.warehouseFreezerDataset });
 })
 
 module.exports = router;
